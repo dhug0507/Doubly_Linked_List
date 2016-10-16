152597237233369a1f4f54474db6da88c366f202
Daniel Hug <dhug@albany.edu>
2016-10-16 00:02:04 -0400

    Created DList class, declared the variable head and last, and created a constructor

---
+++ Linked_ListHW/src/DList.java
@@ -0,0 +1,27 @@
+/*
+ * Daniel Hug
+ * Linked List Homework
+ * 10/14/16
+ * Part of my code is reference from my textbook "Starting out with Java
+ * from Control structures to Data Structures"
+ */
+
+/**
+ * The DList class contains methods for implementing a doubly
+ * linked list.
+ */
+public class DList {
+	public Node head; //Head of the list
+	public Node last; //Last element in the list
+	
+	/**
+	 * Constructor
+	 */
+	public DList(){
+		head = null;
+		last = null;
+	}
+	
+	
+
+}
